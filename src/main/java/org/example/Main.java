719d1d3c470c73a1d9f822652c477973b0c1ae4f
Jared P <JaredP030@gmail.com>
2025-02-27 08:15:54 -0500

    Main.java commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 2;
+
+        calculator.add(a,b);
+        calculator.subtract(a, b);
+        calculator.multiply(a, b);
+        calculator.subtract(a, b);
+    }
+}
