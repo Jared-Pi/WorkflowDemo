45e6b20902a72d9ef8e121825c5841378d0964cc
Jared P <JaredP030@gmail.com>
2025-02-27 08:33:28 -0500

    demo commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        int a = 5;
+        int a = 10;
         int b = 2;
 
         System.out.println("Addition: " + calculator.add(a, b));
